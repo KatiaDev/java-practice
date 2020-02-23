a35c564c7be0516b9c37c296621be8e6a82e75f0
Ekaterina173 <51832550+Ekaterina173@users.noreply.github.com>
2020-02-23 12:21:22 +0200

    Adding the first java file

---
+++ Main.java
@@ -0,0 +1,27 @@
+﻿public class Main{
+   
+       public static void main(String[] args){
+
+//1.Creați 3 variabile pentru fiecare tip primitiv, atribuiți o valoare imediată 
+     
+       int myNumber = 69;
+       char anySymbol = 'Z';
+       double totalCost = 11.659;
+
+//2.Creați 3 variabile pentru fiecare tip primitiv, atribuiți valoarea printr-o instrucțiune separată
+
+       boolean existValue;
+       long nextValue;
+       byte lastValue;
+
+       existValue = false;
+       nextValue = 356000000L;
+       lastValue = 102;
+
+//3.Creați 3 variabile pentru fiecare tip primitiv, atribuiți o valoare oricărei operații matematice
+
+       float summValue = 35+14+7;
+
+      }
+
+}
