07fb995528a164ec448d912eff46b66cc49bb61d
Ekaterina173 <ecaterina.popa18@gmail.com>
2020-02-24 20:31:48 +0200

    IffElse adding

---
+++ Switch.java
@@ -0,0 +1,23 @@
+public class Switch{
+
+  public static void main (String[] args){
+
+  char litera = 'e';
+
+  switch(litera){
+  
+  case 'a': 
+  case 'e': 
+  case 'i': 
+  case 'o': 
+  case 'u': {System.out.println(litera + " este o vocala"); break;}
+  
+   default: {System.out.println(litera + " este o consoana");}
+
+     }
+
+
+   }
+
+
+}
